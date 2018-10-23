ba6d1dd93f3ded2a92fbb2b8af030391fa318a2a
Elias Reis <eliasreis54@gmail.com>
2018-10-23 12:44:17 -0300

    improve router

--- src/EndPoint.ts
+++ src/EndPoint.ts
@@ -6,13 +6,15 @@ const router = express.Router();
  * Add one endpoints related to health check information.
  * @param healthChecker The HealthChecker object
  */
+
 const addEndPointHealthCheck = (healthChecker: HealthChecker) => {
   router.get('/healthCheck', (req: express.Request, res: express.Response) => {
     res.send(200).json(healthChecker.serviceInfo);
   })
+
+  return router;
 }
 
 export = {
-  addEndPointHealthCheck,
-  router
+  addEndPointHealthCheck
 }
