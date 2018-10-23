999801a9e727e117e80d9aa3124acb0f1ca6388c
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-10-23 11:11:02 -0300

    Update src/EndPoint.ts
    Co-Authored-By: eliasreis54 <eliasreis54@gmail.com>

--- src/EndPoint.ts
+++ src/EndPoint.ts
@@ -4,7 +4,11 @@ import { HealthChecker } from './HealthChecker'
  * Add one endpoints related to health check information.
  * @param app The express application
  */
-const addEndPointHealthCheck = (app: express.Application) => {
+const addEndPointHealthCheck = (app: express.Application, healthChecker: HealthChecker) => {
+  app.get('/healthCheck', (req: express.Request, res: express.Response) => {
+    res.send(200).json(healthChecker.serviceInfo);
+  });
+}
   app.get('/healthCheck', (req: express.Request, res: express.Response) => {
     res.send(200).json(HealthChecker.prototype.serviceInfo);
   });
